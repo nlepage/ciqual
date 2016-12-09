d756a77d61433ebe5ca5f753fbbb64f7a2b016f6
nlepage <nicolas.lepage@zenika.com>
2016-12-09 10:05:19 +0100

    Test avec typeahead

--- ciqual.js
+++ ciqual.js
@@ -1,9 +1,27 @@
-var app = angular.module("ciqual", [ "ngResource" ]);
+var app = angular.module("ciqual", [ "ngResource", "siyfion.sfTypeahead" ]);
 
 app.factory("Ingredients", [ "$resource" , function($resource) {
 	return $resource("ciqual.json");
 } ]);
 
 app.controller("CiqualController", [ "$scope", "Ingredients", function($scope, Ingredients) {
-	$scope.ingredients = Ingredients.query();
+	$scope.ingredients = [];
+	$scope.selectedIngredient = null;
+
+	var ingredients = new Bloodhound({
+		datumTokenizer : function(d) { return Bloodhound.tokenizers.whitespace(d.libelle); },
+		queryTokenizer : Bloodhound.tokenizers.whitespace,
+		identify : function(d) { return d.id; },
+		prefetch : "ciqual.json"
+	});
+
+	$scope.ingredientsDataset = {
+		displayKey : 'libelle',
+		source : ingredients.ttAdapter()
+	};
+
+	// FIXME Typeahead options object
+	$scope.exampleOptions = {
+		highlight: true
+	};
 } ])
